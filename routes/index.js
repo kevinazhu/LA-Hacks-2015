2c9d79ce0b0c25d814e0af4b1c9d5a6f0baf0be3
Kevin Zhu <kevinazhu@gmail.com>
2015-04-04 06:41:19 -0700

    fixed routes and titles

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Landing Page' });
 });
 
 router.get('/login', function(req, res) {
   res.render('login', { title: 'Respoke Login' });
+});
+
+router.get('/grouppage', function(req, res) {
+    res.render('grouppage', { title: 'Groups Page' });
 });
 
 module.exports = router;
