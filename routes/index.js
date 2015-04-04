4785b5fc9f66ed802845d701281f9223180d4de2
Kevin Zhu <kevinazhu@gmail.com>
2015-04-04 02:37:51 -0700

    added back basic home page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/chat', function(req, res) {
   res.render('chat', { title: 'Respoke Chat' });
 });
 
