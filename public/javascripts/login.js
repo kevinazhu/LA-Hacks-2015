7a79021b9b9f5435fa32a2f8f4be9e496c7b3636
Kevin Zhu <kevinazhu@gmail.com>
2015-04-04 20:05:31 -0700

    added basic group chat service

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,27 +1,13 @@
-var chat_app = angular.module('loginApp',[]);
+var login_app = angular.module('loginApp',[]);
 
-chat_app.controller('loginCtrl', function($scope) {
-    // here's the App ID value from the portal:
-    var appid = "7caf8bfa-47a2-4113-a899-1a4098174557";
-
-    // create a client object using the App ID value from Step 2
-    var client = respoke.createClient({
-        appId: appid,
-        developmentMode: true
-    });
-
-    $scope.connectionStatus = "Not Connected";
-
-    // listen for the 'connect' event
-    client.listen('connect', function () {
-        $scope.connectionStatus = "Connected to Respoke!";
-    });
-
-    // now connect when the user clicks the 'Connect' button
+login_app.controller('loginCtrl', function($scope, $http) {
     $scope.login = function() {
-        var endpoint = $scope.form.username;
-        client.connect({
-            endpointId: endpoint // your username is the endpoint
-        });
-    };
+        $http.post('/trytologin', {username: $scope.form.username})
+            .success(function() {
+                window.location = "/chat";
+            })
+            .error(function(){
+                console.log("POST ERROR");
+            });
+    }
 });
